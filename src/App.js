fac691c3a3c5beaab9e0e9731881973d32989390
Khushboo Choudhary <khushboochoudhary1409@gmail.com>
2022-09-15 23:01:35 +0530

    Audio player

--- src/App.js
+++ src/App.js
@@ -4,54 +4,58 @@ import { Container, Drawer } from "@material-ui/core";
 import DrawerMenu from "./Components/DrawerMenu";
 import Image from "material-ui-image";
 import Terms from "./Components/TermsAndCondition";
+import AudioPlayer from "./Components/AudioPlayer/AudioPlayer";
+import tracks from "./Components/AudioPlayer/tracks";
+
 function App() {
   return (
     <div className="App">
-      <Container
-        maxWidth="md"
-        style={{
-          color: "#576490",
-          paddingTop: 80,
-          paddingHorizontal: 20,
-          height: "100%",
-          fontWeight: 900,
-        }}
-      >
-        SAIL
-      </Container>
-      {/* <Container
-        maxWidth="md"
-        style={{
-          color: "#576490",
-          paddingTop: 8,
-          paddingHorizontal: 20,
-          height: "100%",
-        }}
-      >
-        (Sentiment Analysis And Interventions)
-      </Container> */}
-      <Container
-        maxWidth="md"
-        style={{
-          color: "#576490",
-          paddingTop: 8,
-          paddingHorizontal: 20,
-          height: "100%",
-          fontWeight: 200,
-          fontSize: 12,
-        }}
-      >
-        Welcome to SAIL , We have curated multiple fun sessions for you to
-        explore. All the personal data gatherd and the results are confidential.
-      </Container>
-      <Container>
-        <Image
-          src={require("../src/assets/images/Psychologist.gif")}
-          style={{ paddingLeft: 20 }}
-        />
-      </Container>
-      <Terms></Terms>
-      <DrawerMenu></DrawerMenu>
+      {/*<Container*/}
+      {/*  maxWidth="md"*/}
+      {/*  style={{*/}
+      {/*    color: "#576490",*/}
+      {/*    paddingTop: 80,*/}
+      {/*    paddingHorizontal: 20,*/}
+      {/*    height: "100%",*/}
+      {/*    fontWeight: 900,*/}
+      {/*  }}*/}
+      {/*>*/}
+      {/*  SAIL*/}
+      {/*</Container>*/}
+      {/*/!* <Container*/}
+      {/*  maxWidth="md"*/}
+      {/*  style={{*/}
+      {/*    color: "#576490",*/}
+      {/*    paddingTop: 8,*/}
+      {/*    paddingHorizontal: 20,*/}
+      {/*    height: "100%",*/}
+      {/*  }}*/}
+      {/*>*/}
+      {/*  (Sentiment Analysis And Interventions)*/}
+      {/*</Container> *!/*/}
+      {/*<Container*/}
+      {/*  maxWidth="md"*/}
+      {/*  style={{*/}
+      {/*    color: "#576490",*/}
+      {/*    paddingTop: 8,*/}
+      {/*    paddingHorizontal: 20,*/}
+      {/*    height: "100%",*/}
+      {/*    fontWeight: 200,*/}
+      {/*    fontSize: 12,*/}
+      {/*  }}*/}
+      {/*>*/}
+      {/*  Welcome to SAIL , We have curated multiple fun sessions for you to*/}
+      {/*  explore. All the personal data gatherd and the results are confidential.*/}
+      {/*</Container>*/}
+      {/*<Container>*/}
+      {/*  <Image*/}
+      {/*    src={require("../src/assets/images/Psychologist.gif")}*/}
+      {/*    style={{ paddingLeft: 20 }}*/}
+      {/*  />*/}
+      {/*</Container>*/}
+      {/*<Terms></Terms>*/}
+      {/*<DrawerMenu></DrawerMenu>*/}
+        <AudioPlayer tracks={tracks} />
     </div>
   );
 }
